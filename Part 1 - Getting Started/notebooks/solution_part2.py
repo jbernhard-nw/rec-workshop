02a82d14094a2822b3c1cb6decb6af2e411c9492
Josh Bernhard <jbernhard@nerdwallet.com>
2020-02-09 16:26:10 -0800

    update second notebook and solutions to part 1

---
+++ Part 1 - Getting Started/notebooks/solution_part2.py
@@ -0,0 +1,46 @@
+from IPython.display import Image
+
+
+def answer_one(your_answer):
+    '''
+    your answer to question 1 should be a letter
+    a to e
+    '''
+    if your_answer == 'serendipity':
+        return Image(filename='../../images/you_right.png')
+
+    else:
+        print("Sorry, that's not correct.  Try again!")
+
+def answer_two(your_answer):
+    '''
+    your answer to question 1 should be a letter
+    a to e
+    '''
+    if your_answer == 'relevance':
+        return Image(filename='../../images/yup.png')
+
+    else:
+        print("Sorry, that's not correct.  Try again!")
+
+def answer_three(your_answer):
+    '''
+    your answer to question 1 should be a letter
+    a to e
+    '''
+    if your_answer == 'the cold start problem':
+        return Image(filename='../../images/you_got_it.png')
+
+    else:
+        print("Sorry, that's not correct.  Try again!")
+
+def answer_four(your_answer):
+    '''
+    your answer to question 1 should be a letter
+    a to e
+    '''
+    if your_answer == 'novelty':
+        return Image(filename='../../images/good_job.png')
+
+    else:
+        print("Sorry, that's not correct.  Try again!")
